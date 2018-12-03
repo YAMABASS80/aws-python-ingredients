e07af5e828c81840c44ac112f2ea6cfeff8c43ef
Yamabe <myamabe@88e9fe4e36e7.ant.amazon.com>
2018-12-03 11:37:03 +0900

    Add: List Instance Id

--- ec2/list_instance_id/list_instance_id.py
+++ ec2/list_instance_id/list_instance_id.py
@@ -1,3 +1,17 @@
 import boto3
 ec2 = boto3.client('ec2')
 response = ec2.describe_instances()
+
+reservations = []
+for r in response['Reservations']:
+    reservations.append(r)
+
+instances = []
+for i in reservations:
+    instances.extend(i['Instances'])
+
+instance_ids = []
+for i in instances:
+    instance_ids.append(i['InstanceId'])
+
+print(instance_ids)
